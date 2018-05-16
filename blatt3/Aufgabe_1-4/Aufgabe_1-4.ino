b74dec3842122f3fc1589cc9bbcd7651a7b1709a
Maximilian Nazarati <nmaximillian@gmail.com>
2018-05-16 14:06:03 +0200

    aufgabe 2 :(

---
+++ blatt3/Aufgabe_1-4/Aufgabe_1-4.ino
@@ -0,0 +1,37 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(3, 4, 6, 7, 8, 9);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(11, OUTPUT);
+  lcd.begin(2, 8);
+}
+
+// Aufgabe 1
+void setPin11(bool high) {
+  if (high) {
+    PORTB |= (1 << 3);
+  }
+  else {
+    PORTB &= ~(1 << 3);
+  }
+}
+
+// Aufgabe 2
+void setPin11Asm( const bool high) {
+   asm volatile (
+    "start:"
+    "cp %2, %3\n\t"
+    "rjmp %3\n\t"
+    "sbi %0, %1\n\t"
+    "cbi %0, %1\n\t"
+    "rjmp start\n\t"
+    :: "I" (_SFR_IO_ADDR(PORTB)), "I" (PORTB3), "I" (high), "I" (1)
+   );
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  setPin11Asm(false);
+}
