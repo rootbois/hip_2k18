53d0b7da8694318be3847463e80eb027c4e5ee38
vincesolok <spam@kataikko.de>
2018-05-16 13:40:23 +0200

    Aufgabe 3 mit lcd ausgabe

--- blatt3/Aufgabe_1-4/Aufgabe_1-4.ino
+++ blatt3/Aufgabe_1-4/Aufgabe_1-4.ino
@@ -1,4 +1,20 @@
+#include <LiquidCrystal.h>
+
 int c;
+
+// PINS
+#define R_S 3
+#define E   4
+#define DB4 6
+#define DB5 7
+#define DB6 8
+#define DB7 9
+#define NUM_CHAR 20
+#define NUM_LINES 4
+
+// Define the LCD screen
+LiquidCrystal lcd(R_S, E, DB4, DB5, DB6, DB7);
+
 void setPin11(bool high) {
   if (high) {
     PORTB |= (1 << 3);
@@ -43,7 +59,19 @@ void setup() {
   double end_BuiltIn = millis();
   double dur_BuiltIn = end_BuiltIn - start_BuiltIn;
 
-  
+  lcd.begin(NUM_CHAR, NUM_LINES);
+  lcd.setCursor(0,0);
+  lcd.print("With Assembly: ");
+  lcd.setCursor(0,16);
+  lcd.print(dur_Asm);
+  lcd.setCursor(1,0);
+  lcd.print("Without Assembly: ");
+  lcd.setCursor(1,20);
+  lcd.print(dur_Norm);
+  lcd.setCursor(2,0);
+  lcd.print("Built-In: ");
+  lcd.setCursor(2,11);
+  lcd.print(dur_BuiltIn);
 }
 
 void loop() {
